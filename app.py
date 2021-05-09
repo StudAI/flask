20591850d2d3176af016b4c09e56cf42df6dd8cc
Callum Fletcher <callumafletcher@gmail.com>
2021-05-09 00:41:24 -0700

    update

--- app.py
+++ app.py
@@ -7,47 +7,9 @@ import json
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-# @app.route('/getmsg/', methods=['GET'])
-# def respond():
-#     # Retrieve the name from url parameter
-#     name = request.args.get("name", None)
-
-#     # For debugging
-#     print(f"got name {name}")
-
-#     response = {}
-
-#     # Check if user sent a name at all
-#     if not name:
-#         response["ERROR"] = "no name found, please send a name."
-#     # Check if the user entered a number not a name
-#     elif str(name).isdigit():
-#         response["ERROR"] = "name can't be numeric."
-#     # Now the user entered a valid name
-#     else:
-#         response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-#     # Return the response in json format
-#     return jsonify(response)
-
-# @app.route('/post/', methods=['POST'])
-# def post_something():
-#     param = request.form.get('name')
-#     print(param)
-#     # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-#     if param:
-#         return jsonify({
-#             "Message": f"Welcome {param} to our awesome platform!!",
-#             # Add this option to distinct the POST request
-#             "METHOD" : "POST"
-#         })
-#     else:
-#         return jsonify({
-#             "ERROR": "no name found, please send a name."
-#         })
 
 
-# returns the uuid of the next element in arr that matches the cluster of target 
+
 def get_match_id(start_idx, target, arr, data):
     length = len(arr)
     for i in range(start_idx,length):
@@ -56,20 +18,20 @@ def get_match_id(start_idx, target, arr, data):
 
 @app.route('/matches', methods=['POST'])
 def get_matches_array():
+    print(request.json)
+    userData = request.json
     with open('users.json') as f:
         data = json.load(f)
 
-    clusterData = [[1,1,1,1,1,1,1,1,1,1]]
+    clusterData = [[userData["math"],userData["science"],userData["english"],userData["engineering"],userData["grade_level"],userData["extraversion"],userData["agreeableness"],userData["conscientiousness"],userData["neuroticism"],userData["openness"]]]
     for i in data:
         clusterData.append([i["math"], i["science"], i["english"], i["engineering"], i["grade_level"],
                         i["extraversion"], i["agreeableness"], i["conscientiousness"], i["neuroticism"], i["openness"]])
     clusterData = np.array(clusterData)
 
-    # km is a numpy array where index = index in users and the value = cluster number
-    km = AgglomerativeClustering(n_clusters=12).fit_predict(clusterData)
+    km = AgglomerativeClustering(n_clusters=100).fit_predict(clusterData)
     print(km)
     
-    # the response this request should return -> an array of dictionaries where each dictionary is in the format of { person1_uuid: person2_uuid }
     res = []
     userCluster = km[0]
 
@@ -78,25 +40,13 @@ def get_matches_array():
             res.append({"id": data[i]["user_id"]})
     
     return jsonify({
-        "matches": res[0:5]
+        "matches": res[0:userData["numberOfMatches"]]
     })
-    # if clusterData:
-    #     return jsonify({
-    #         "Message": f"Welcome {param} to our awesome platform!!",
-    #         # Add this option to distinct the POST request
-    #         "METHOD" : "POST"
-    # })
-    # else:
-    #     return jsonify({
-    #         "ERROR": "no name found, please send a name."
-    #     })
-        
 
-# A welcome message to test our server
+
 @app.route('/')
 def index():
     return "<h1>Welcome to our server !!</h1>"
 
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
